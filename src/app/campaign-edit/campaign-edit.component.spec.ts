b1c1e0896cd98e62417af49d057744368c563dda
DESKTOP-4AG0LQQ\hvrooyen <akkedis123@gmail.com>
2018-11-11 14:28:34 +0100

    add unit tests

--- src/app/campaign-edit/campaign-edit.component.spec.ts
+++ src/app/campaign-edit/campaign-edit.component.spec.ts
@@ -6,14 +6,19 @@ import {
   MatCheckboxModule,
   MatDatepickerModule,
   MatFormFieldModule,
-  MatInputModule, MatOptionModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatOptionModule,
   MatRadioModule,
   MatSelectModule
 } from '@angular/material';
-import {ReactiveFormsModule} from '@angular/forms';
-import {RouterTestingModule} from '@angular/router/testing';
-import {CampaignsService} from '../campaigns.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CampaignsService } from '../campaigns.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable } from 'rxjs';
+import 'rxjs/add/observable/of';
 
 describe('CampaignEditComponent', () => {
   let component: CampaignEditComponent;
@@ -27,12 +32,14 @@ describe('CampaignEditComponent', () => {
         MatFormFieldModule,
         MatInputModule,
         MatDatepickerModule,
+        MatNativeDateModule,
         MatCheckboxModule,
         MatRadioModule,
         MatSelectModule,
         MatOptionModule,
         RouterTestingModule,
         HttpClientTestingModule,
+        BrowserAnimationsModule
       ],
       declarations: [ CampaignEditComponent ],
       providers: [CampaignsService]
@@ -43,11 +50,71 @@ describe('CampaignEditComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CampaignEditComponent);
     component = fixture.componentInstance;
-    const campaignService = fixture.debugElement.injector.get(CampaignsService);
     fixture.detectChanges();
+
+    const campaignService = fixture.debugElement.injector.get(CampaignsService);
+    spyOn(campaignService, 'getCampaignsById').and.returnValue(Observable.of({
+      PlistaProduct: 'Product 8',
+      a: 8,
+      camp_cpc: 0.271,
+      campaignid: '210a0e48ec0aea780bb747fe56a72d92',
+      date: '2016-02-29T10:00:06',
+      freeclick: true,
+      frienddomainid: '9a8799e6ac57db61203d74b98d78e2ee',
+      network: 'b',
+      userid: '16d798b3ae9d72408a91a8737bedb661'
+    }));
+    component.ngOnInit();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('form invalid when empty', () => {
+    expect(component.form.valid).toBeTruthy();
+  });
+
+  it('camp_cpc field validity', () => {
+    const camp_cpc = component.form.controls['camp_cpc'];
+    expect(camp_cpc.valid).toBeTruthy();
+  });
+
+  it('date group validity', () => {
+    const date = component.form.controls['datetime'];
+    expect(date.valid).toBeTruthy();
+  });
+
+  it('freeclick field validity', () => {
+    const freeclick = component.form.controls['freeclick'];
+    expect(freeclick.valid).toBeTruthy();
+  });
+
+  it('network field validity', () => {
+    const network = component.form.controls['network'];
+    expect(network.valid).toBeTruthy();
+  });
+
+  it('PlistaProduct field validity', () => {
+    const PlistaProduct = component.form.controls['PlistaProduct'];
+    expect(PlistaProduct.valid).toBeTruthy();
+  });
+
+  it('camp_cpc field validity with no data', () => {
+    component.form.patchValue({'camp_cpc': ''});
+    const camp_cpc = component.form.controls['camp_cpc'];
+    expect(camp_cpc.valid).toBeFalsy();
+  });
+
+  it('date group validity with no data', () => {
+    component.form.patchValue({'datetime': {'date': '', 'time': ''}});
+    const date = component.form.controls['datetime'];
+    expect(date.valid).toBeFalsy();
+  });
+
+  it('PlistaProduct field validity with no data', () => {
+    component.form.patchValue({'PlistaProduct': ''});
+    const PlistaProduct = component.form.controls['PlistaProduct'];
+    expect(PlistaProduct.valid).toBeFalsy();
   });
 });
--- src/app/campaigns/campaigns.component.spec.ts
+++ src/app/campaigns/campaigns.component.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CampaignsComponent } from './campaigns.component';
-import {MatPaginatorModule, MatTableModule} from '@angular/material';
-import {RouterTestingModule} from '@angular/router/testing';
-import {CampaignsService} from '../campaigns.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatPaginatorModule, MatTableModule } from '@angular/material';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CampaignsService } from '../campaigns.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable } from 'rxjs';
 
 describe('CampaignsComponent', () => {
   let component: CampaignsComponent;
@@ -30,9 +31,32 @@ describe('CampaignsComponent', () => {
     fixture = TestBed.createComponent(CampaignsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    const campaignService = fixture.debugElement.injector.get(CampaignsService);
+    spyOn(campaignService, 'getCampaigns').and.returnValue(Observable.of([{
+      PlistaProduct: 'Product 8',
+      a: 8,
+      camp_cpc: 0.271,
+      campaignid: '210a0e48ec0aea780bb747fe56a72d92',
+      date: '2016-02-29T10:00:06',
+      freeclick: true,
+      frienddomainid: '9a8799e6ac57db61203d74b98d78e2ee',
+      network: 'b',
+      userid: '16d798b3ae9d72408a91a8737bedb661'
+    }]));
+    component.ngOnInit();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('datagrid should not be empty', () => {
+    expect(component.dataSource.data.length).toEqual(1);
+  });
+
+  it('datagrid should display table rows(header and content)', () => {
+    const view = fixture.debugElement.nativeElement;
+    expect(view.querySelectorAll('tr').length).toEqual(2);
   });
 });
